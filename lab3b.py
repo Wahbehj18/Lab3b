e0f482fff9f2493c37bc1871827520a6478881e0
Valentin Isaac Lagunes <valentin@lnxsrv09.seas.ucla.edu>
2020-03-09 10:32:43 -0700

    started block audit

--- lab3b.py
+++ lab3b.py
@@ -1,7 +1,17 @@
 import csv
 import argparse
+import sys
+group = None
+blockSet = set([])
+inodeSet = set([])
+freeInodes = []
+inodeList = []
+direntList = []
+IndirectList = []
+bfreeList = []
+superBlockObject = None
+usedBlocks = {}
 
-    
 class Superblock:
     def __init__(self, column):
         self.BlockTotal = int(column[1])
@@ -12,8 +22,8 @@ class Superblock:
         self.InodesPerGroup = int(column[6])
         self.FirstNonReservedInode = int(column[7])
 
-    def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+#    def __str__(self):
+#        return str(self.__class__) + ": " + str(self.__dict__)
 
 class Group:
     def __init__(self, column):
@@ -29,23 +39,77 @@ class Group:
 
 class Inode:
     def __init__(self, column):
-        self.InodeNumber = int(column[1])
-        self.fileType = str(column[2])
-
+        self.inodeNumber = int(column[1])
+        self.fileType = column[2]
+        self.mode  = int(column[3])
+        self.owner = int(column[4])
+        self.group = int(column[5])
+        self.linkCount = int(column[6])
+        self.LastChange = column[7]
+        self.ModificationTime = column[8]
+        self.lastAccess = column[9]
+        self.fileSize = int(column[10])
+        self.DiskSpace = int(column[11])
+        self.directBlocks = []
+        self.indirectBlocks = []
+        if self.fileType == 'f' or self.fileType == 'd':
+            for x in range(12,24):
+                self.directBlocks.append(int(column[x]))
+            for x in range(24,27):
+                self.indirectBlocks.append(int(column[x]))
+        
         
 class Block:
-    def __init__(self, blockNumber, indirect, free, offset):
+    def __init__(self, blockNumber, indirect, free, offset, inodeNum):
         self.blockNumber = blockNumber
         self.indirection = indirect # 0-direct, 1-single ...
         self.free = free
         self.offset = offset
+        self.inodeNum = inodeNum
 
         
 class Dirent:
     def __init__(self, column):
-        self.Name = int(column[6])
+        self.ParentInode = int(column[1])
+        self.ByteOffset = int(column[2])
+        self.InodeNum = int(column[3])
+        self.EntryLength = int(column[4])
+        self.NameLength = int(column[5])
+        self.Name = column[6]
+
+class Indirect:
+    def __init__(self, column):
+        self.OwnerInode = int(column[1])
+        self.IndirectionLevel = int(column[2])
+        self.BlockOffset = int(column[3])
+        self.BlockNum = int(column[4])
+        self.BlockNumRef = int(column[5])
 
 
+def BlockAudit(superBlock, groupObj):
+    MaxBlocks = superBlock.BlockTotal
+    FirstBlock = int(groupObj.FirstInodeBlockNum + superBlock.InodeSize * groupObj.TotalInodes / superBlock.blockSize)
+    for x in inodeList:
+        logicalOffset = 0
+        for block in x.directBlocks:
+            if block < 0 or block >= MaxBlocks:
+                print("INVALID BLOCK {} IN INODE {} AT OFFSET {}".format(block, x.inodeNumber, logicalOffset))
+            elif block != 0 and block < FirstBlock:
+                print("RESERVED BLOCK {} IN INODE {} AT OFFSET {}".format(block, x.inodeNumber, logicalOffset))
+            elif block != 0:
+                if block not in usedBlocks:
+                    usedBlocks[block] = [Block(block, 0, False, logicalOffset, x)]
+                else:
+                    usedBlocks[block].append(Block(block, 0, False, logicalOffset, x))             
+            logicalOffset += 1
+    return
+
+
+def InodeAudit():
+    return
+
+def DirectoryAudit():
+    return
 
 def main():
     parser = argparse.ArgumentParser(description='Parse CSV file')
@@ -64,22 +128,30 @@ def main():
         return 1
 
     fileReader = csv.reader(csvFile)
-    group = []
-    blockSet = set([])
-    inodeSet = set([])
+       
     for row in fileReader:
-        #print(','.join(row))
         if row[0] == "SUPERBLOCK":
-            super = Superblock(row)
+            superBlockObject = Superblock(row)
         elif row[0] == "BFREE":
-            blockSet.add(Block(int(row[1]), 0, True, 0))
+            bfreeList.append(int(row[1]))
+            #blockSet.add(Block(int(row[1]), 0, True, 0))
         elif row[0] == "INDIRECT":
-            blockSet.add(Block(int(row[5]), int(row[2]), False, int(row[4])))
-            
+            IndirectList.append(Indirect(row))
+            #blockSet.add(Block(int(row[5]), int(row[2]), False, int(row[4])))
+        elif row[0] == "GROUP":
+            group = Group(row)
+        elif row[0] == "IFREE":
+            freeInodes.append(int(row[1]))
+            inodeSet.add(int(row[1]))
+        elif row[0] == "INODE":
+            inodeList.append(Inode(row))
+        elif row[0] == "DIRENT":
+            direntList.append(Dirent(row))
         
     for b in blockSet:
         print(b.blockNumber)
 
+    BlockAudit(superBlockObject, group)
             
 
 if __name__ == '__main__':
