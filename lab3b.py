b927ef7bba6f9dcb0440b93ee486a9637b3126e4
Valentin Isaac Lagunes <valentin@lnxsrv09.seas.ucla.edu>
2020-03-08 18:34:16 -0700

    started classes

--- lab3b.py
+++ lab3b.py
@@ -16,8 +16,45 @@ def checkArgs():
     except IOError:
         print("Error File does not exist")
         return 1
+class Superblock:
+    def __init__(self, column):
+        self.BlockTotal = int(column[1])
+        self.InodeTotal = int(column[2])
+        self.blockSize  = int(column[3])
+        self.InodeSize  = int(column[4])
+        self.BlocksPerGroup = int(column[5])
+        self.InodesPerGroup = int(column[6])
+        self.FirstNonReservedInode = int(column[7])
+
+class Group:
+    def __init__(self, column):
+        self.GroupNum = int(column[1])
+        self.TotalBlocks = int(column[2])
+        self.TotalInodes  = int(column[3])
+        self.NumFreeBlocks = int(column[4])
+        self.NumFreeInodes = int(column[5])
+        self.BlockBitmap = int(column[6])
+        self.InodeBitmap = int(column[7])
+        self.FirstInodeBlockNum = int(column[8])
 
 
+class Inode:
+    def __init__(self, column):
+        self.InodeNumber = int(column[1])
 
-checkArgs()
+
+class Dirent:
+    def __init__(self, column):
+        self.Name = int(column[6])
+
+class Indirect:
+    def __init__(self, column):
+        self.InodeNumber = int(column[1])
+
+
+def main():
+    checkArgs()
+
+if __name__ == '__main__':
+    main()
 
